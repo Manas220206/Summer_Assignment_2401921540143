73e5f7df8d12faacfd729bd700e232b6c179652e
Manas <manas220206@gmail.com>
2026-06-08 22:35:26 +0530

    WEEK 2 DAY 1 ASSIGNMENT DONE

---
+++ Week_2/Day_1/Q1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] x=new int[26];
+        int[] y=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            x[ch-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            y[ch-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(x[i]!=y[i])return false;
+        }
+    return true;}
+}
